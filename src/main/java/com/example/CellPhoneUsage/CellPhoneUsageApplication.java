75e9937e0eac8fa70b253f93b129e140d3bff248
Bernabé Garro <bgarro@ceroriesgo.co.cr>
2021-08-15 19:40:47 -0600

    initializing app

---
+++ src/main/java/com/example/CellPhoneUsage/CellPhoneUsageApplication.java
@@ -0,0 +1,13 @@
+package com.example.CellPhoneUsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellPhoneUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellPhoneUsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CellPhoneUsage/CellPhoneUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CellPhoneUsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellPhoneUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
